42114a239b0545827a2ce728cb1604baa7911f81
ilarinie <ilari.nieminen@outlook.com>
2017-06-12 17:09:30 +0300

    Trying to get server.js to work

--- server.js
+++ server.js
@@ -4,21 +4,6 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-
-app.use(forceSSL());
-
-
-
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
