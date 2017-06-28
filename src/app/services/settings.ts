a1bc3141a06d018cbd4785b9a19f93ff92281016
ilarinie <ilari.nieminen@outlook.com>
2017-06-28 13:23:38 +0300

    frontpage initial

---
+++ src/app/services/settings.ts
@@ -0,0 +1,19 @@
+export class Settings {
+
+  /*
+    //// VIEWS ////
+
+    1 - BUDGET
+    2 - NEWPURCHASE
+    3 - PROFILE
+    4 - TASKS
+    5 - USERS
+   */
+   views: string[] = [
+     "<app-budget></app-budget>",
+     "<app-new-purchase></app-new-purchase>"
+     "<"
+   ]
+   selected: number[];
+
+}
